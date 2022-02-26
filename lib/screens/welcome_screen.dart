bb9efcc26c199a74e55b1a1a846d0eaa7155ab2a
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-26 10:53:21 -0500

    Basic install of an Animation Controller with Mixin.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -11,7 +11,26 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController controller;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+
+    controller.forward();
+
+    controller.addListener(() {
+      print(controller.value);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
